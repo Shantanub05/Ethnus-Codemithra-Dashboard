6639bf575b5d4b562a210c0784cd5d64f4c07467
Shantanub05 <shantanubokey@gmail.com>
2024-07-07 21:42:03 +0530

    url update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000/api/v1",
-  baseURL: "https://ethnus-codemithra-dashboard.onrender.com",
+  baseURL: "https://ethnus-codemithra-dashboard.onrender.com/api/v1",
 });
 
 export default API;
