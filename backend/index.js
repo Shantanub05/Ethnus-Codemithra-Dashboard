c00d0fc9fca452ffdbe07bf4cf339e3a32ae2abd
Shantanub05 <shantanubokey@gmail.com>
2024-07-07 21:51:56 +0530

    url update

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,11 @@ connectDB();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://ethnus-codemithra-dashboard-84n4dz3i0.vercel.app", // Replace with your Vercel domain
+  })
+);
 
 // Use routes
 app.use("/api/v1/product", productRoutes);
@@ -23,8 +27,8 @@ app.use("/api/v1/analytics", analyticsRoutes);
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack); 
-  res.status(500).send(err.message); 
+  console.error(err.stack);
+  res.status(500).send(err.message);
 });
 
 const PORT = process.env.PORT || 5000;
