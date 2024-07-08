836bd30abda74567e710633bc1f4234342a5a7dc
Shantanub05 <105590952+Shantanub05@users.noreply.github.com>
2024-07-08 15:29:45 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   // baseURL: "http://localhost:5000/api/v1",
   // baseURL: "https://ethnus-codemithra-dashboard.onrender.com",
-  baseURL: "http://ethnus-codemithra-dashboard-production.up.railway.app",
+  baseURL: "https://ethnus-codemithra-dashboard-production.up.railway.app",
 });
 
 export default API;
